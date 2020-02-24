efc4597a1b6e3257fbd6011535fa996da89da7ed
Thomas Boerger <thomas@webhippie.de>
2020-02-24 12:47:59 +0100

    Refactor structure similar to the other roles

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,29 @@
+import os
+import pytest
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']
+).get_hosts('all')
+
+
+@pytest.mark.parametrize('package', [
+    "apt-transport-https",
+    "software-properties-common",
+    "tree",
+    "htop",
+    "iotop",
+    "sysstat",
+    "ipvsadm",
+    "update-notifier-common",
+    "socat",
+    "jq",
+    "curl",
+    "apt-dater-host",
+    "net-tools",
+    "traceroute"
+])
+def test_is_installed(host, package):
+    pkg = host.package(package)
+    assert pkg.is_installed
